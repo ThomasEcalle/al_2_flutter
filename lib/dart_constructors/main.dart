fc17d3df0df8925ad42c8824c088e9818feb56d2
Thomas Ecalle <thomasecalle@gmail.com>
2022-10-10 11:04:25 +0200

    Add MaterialApp lesson

--- lib/dart_constructors/main.dart
+++ lib/dart_constructors/main.dart
@@ -1,13 +1,14 @@
 void main() {
   const a = User(firstName: "Thomas");
-  const b = User(firstName: "bob");
+  const b = User(firstName: "Thomas");
 
   print(a == b);
 }
 
-
 class User {
   final String firstName;
 
-  const User({required this.firstName});
+  const User({this.firstName = ''});
+
+  const User.withoutName({this.firstName = ''});
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container(color: Colors.amber));
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            fontSize: 24,
+            color: Colors.green,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+      child: Center(
+        child: Text(
+          'Coucou',
+          style: Theme.of(context).textTheme.headline1,
+        ),
+      ),
+    );
+  }
+}
